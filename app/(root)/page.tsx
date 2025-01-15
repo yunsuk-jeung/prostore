f30a0f46ebe7f295e86c3bae8ead32149d72722f
ysjeung <yunsukjeung@snu.ac.kr>
2025-01-15 21:55:23 +0900

    end of section2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 export const metadata = {
   title: 'Home',
 };
 const HomePage = () => {
-  return <>ProsStore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList>
+    </>
+  );
 };
 
 export default HomePage;

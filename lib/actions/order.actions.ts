1a22b21a666c4944081c0e0911376f6d76b59ed2
ysjeung <yunsukjeung@snu.ac.kr>
2025-03-21 15:17:07 +0900

    added stripe webhook endpoint

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -191,7 +191,7 @@ export async function approvePaypalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
